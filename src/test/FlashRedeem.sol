1902a5bb22655b7b95d0bd8264ff6283b3fcce83
jazzshow <jazzshow2017@gmail.com>
2022-09-25 10:06:05 +0800

    update

--- src/test/FlashRedeem.sol
+++ src/test/FlashRedeem.sol
@@ -4,22 +4,24 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "forge-std/console.sol";
 import "../FlashRedeem.sol";
-import { IERC721Upgradeable } from "nftx/token/IERC721Upgradeable.sol";
+import {IERC721Upgradeable} from "nftx/token/IERC721Upgradeable.sol";
 
 interface CheatCodes {
     function startPrank(address) external;
+
     function stopPrank() external;
 }
 
 contract FlashRedeemTest is DSTest {
-
-    address private DOODLES_NFTX_ADDR = 0x2F131C4DAd4Be81683ABb966b4DE05a549144443;
-    address private DOODLES_NFT_ADDR = 0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e;
+    address private DOODLES_NFTX_ADDR =
+        0x2F131C4DAd4Be81683ABb966b4DE05a549144443;
+    address private DOODLES_NFT_ADDR =
+        0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e;
     address private SENSEI_ADDR = 0x527B0642b3902C3Bc29ae13D8208b86dA007aa26;
 
     FlashRedeem flashRedeem;
-    IERC721Upgradeable private DOODLES_NFT = IERC721Upgradeable(DOODLES_NFT_ADDR);
-
+    IERC721Upgradeable private DOODLES_NFT =
+        IERC721Upgradeable(DOODLES_NFT_ADDR);
 
     function setUp() public {
         flashRedeem = new FlashRedeem();
@@ -28,7 +30,7 @@ contract FlashRedeemTest is DSTest {
     function testFlashRedeem() public {
         // 5.2e18 (1e18 is decimal configuration for BAYC Token - flash loan 5.2 tokens)
         // why 5.2? NFTX vault charges 4% redeem fee, 5.2 allows us to redeem exactly 5 NFTs
-        uint256 amount =  10400000000000000000;
+        uint256 amount = 10400000000000000000;
         address flashRedeemContractAddr = address(flashRedeem);
         console.log("flashRedeemContractAddr", flashRedeemContractAddr);
 
@@ -36,7 +38,7 @@ contract FlashRedeemTest is DSTest {
         cheats.startPrank(SENSEI_ADDR);
 
         DOODLES_NFT.setApprovalForAll(flashRedeemContractAddr, true);
-        flashRedeem.flashBorrow{value: 8.5 ether}(DOODLES_NFTX_ADDR, amount);
+        flashRedeem.flashBorrow{value: 7.5 ether}(DOODLES_NFTX_ADDR, amount);
 
         cheats.stopPrank();
     }
